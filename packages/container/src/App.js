d28f041c416814880b84921ce0ae53476231a769
shermondevelopment <victorshermon@yahoo.com>
2022-06-11 15:59:43 -0300

    phiysis

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -5,7 +5,7 @@ import MarketingApp from '../components/MarketingApp'
 console.log(mount)
 export default () => {
   return <div>
-      <h2>Hi name!</h2>
+      <h2>Hi storre!</h2>
       <hr />
       <MarketingApp />
     </div>
