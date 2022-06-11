c04cd10180e3eb381949ba28fece12829a31e4d0
shermondevelopment <victorshermon@yahoo.com>
2022-06-11 16:39:56 -0300

    update

--- packages/container/config/webpack.prod.js
+++ packages/container/config/webpack.prod.js
@@ -10,7 +10,7 @@ const prodConfig = {
   mode: 'production',
   output: {
     filename: '[name].[contenthash].js',
-    publicPath: '/container/lates/'
+    publicPath: '/container/latest/'
   },
   plugins: [
     new ModuleFederationPlugin({
