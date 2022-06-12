2e6571b0fab9a2671f45c4f32620ed5c88549c9c
shermondevelopment <victorshermon@yahoo.com>
2022-06-11 23:18:13 -0300

    deploy 2

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -3,15 +3,21 @@ import { mount } from 'marketing/MarketingApp'
 import MarketingApp from '../components/MarketingApp'
 import { Router, BrowserRouter } from 'react-router-dom'
 import Header from '../components/Header'
+import { StylesProvider, createGenerateClassName } from '@material-ui/styles'
 
 console.log(mount)
+const generateClassName = createGenerateClassName({
+  productionPrefix: 'co'
+})
 export default () => {
   return (
     <>
+    <StylesProvider generateClassName={generateClassName}>
       <BrowserRouter>
-        <Header />
-        <MarketingApp />
-      </BrowserRouter>
+          <Header />
+          <MarketingApp />
+        </BrowserRouter>
+    </StylesProvider>
     </>
   )
 }
